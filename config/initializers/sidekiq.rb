dfcc4ed84a176da1585c3c73504166f88730ac07
Luis Gonzalez <intellto24@gmail.com>
2018-05-03 08:52:50 +0100

    heroku testing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  config.redis = {  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 5 }
+  config.redis = {  }
 end
