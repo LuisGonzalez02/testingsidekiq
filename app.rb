da4b852712aa29733c50e32f7c476013ab74449c
Luis Gonzalez <intellto24@gmail.com>
2018-05-03 10:44:36 +0100

    heroku testing

--- app.rb
+++ app.rb
@@ -3,30 +3,11 @@ require 'sidekiq'
 require 'sidekiq/api'
 require 'sidekiq/web'
 
+require'./lib/workers/test_worker.rb'
 
 
 
 class App < Sinatra::Base
-	class TestWorker
-	include Sidekiq::Worker
-		def perform(sleep_time)
-			sleep sleep_time
-			account_sid = 'AC3040bfd66e0c15eeae03e6b7bd55443c'
-			auth_token = 'a10018bf4cfb66aa234e84b45960c3f5'
-
-			# set up a client to talk to the Twilio REST API
-			@client = Twilio::REST::Client.new account_sid, auth_token
-			def ffcaller(calledNumber)
-			call = @client.calls.create(
-			   to: calledNumber,
-			   from: "+12019928984",
-			   url: "https://handler.twilio.com/twiml/EHe5881b22b8f52a84a6b22a72c2882d39")
-			puts call.to
-			end
-			string= "+1#{9566386258}"
-			ffcaller(string)
-		end
-	end
 	get '/' do
 		stats = Sidekiq::Stats.new
 		workers = Sidekiq::Workers.new
--- lib/workers/test_worker.rb
+++ lib/workers/test_worker.rb
@@ -1,4 +1,23 @@
 require 'sidekiq'
 
 
+class TestWorker
+	include Sidekiq::Worker
+	def perform(sleep_time)
+		sleep sleep_time
+		account_sid = 'AC3040bfd66e0c15eeae03e6b7bd55443c'
+		auth_token = 'a10018bf4cfb66aa234e84b45960c3f5'
 
+		# set up a client to talk to the Twilio REST API
+		@client = Twilio::REST::Client.new account_sid, auth_token
+		def ffcaller(calledNumber)
+		call = @client.calls.create(
+		   to: calledNumber,
+		   from: "+12019928984",
+		   url: "https://handler.twilio.com/twiml/EHe5881b22b8f52a84a6b22a72c2882d39")
+		puts call.to
+		end
+		string= "+1#{9566386258}"
+		ffcaller(string)
+	end
+end
